0cdc6f7786ed978c82832abc1082b9f88acd0f8f
muchilafred <f.m.muchila@rgu.ac.uk>
2017-03-21 18:11:53 +0000

    Update home.php

--- home.php
+++ home.php
@@ -4,7 +4,8 @@ include("db_connect.php");
 $username = $_POST["username"];
 $password = $_POST["password"];
 $email_address = $_POST["email_address"];
-$sql_query = "INSERT INTO users (username, password, email_address) VALUES ('$username', '$password', '$email_address')";
+$city = $_POST["city"];
+$sql_query = "INSERT INTO users (username, password, email_address, city) VALUES ('$username', '$password', '$email_address', '$city')";
 if (mysqli_query($link, $sql_query)){
 } else {
     echo "ERROR: ". $sql_query. "<br>" . mysqli_error($link);
