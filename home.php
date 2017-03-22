4633bd67476593d0ec25be439d95a781687a8e5e
muchilafred <f.m.muchila@rgu.ac.uk>
2017-03-22 18:15:17 +0000

    coursework

--- home.php
+++ home.php
@@ -1,15 +1,32 @@
+<html>
+
+<head>
+
+    <title>
+
+
+
+        Admin
+
+
+    </title>
+
+
+</head>
+
+<body>
+
+
 <?php
 
 echo "hello world";
 
 include("dbconnect.php");
-$username = $_POST["username"];
-$password = $_POST["password"];
-$email_address = $_POST["email_address"];
-$city = $_POST["city"];
-$sql_query = "INSERT INTO users (username, password, email_address, city) VALUES ('$username', '$password', '$email_address', '$city')";
-if (mysqli_query($link, $sql_query)){
-} else {
-    echo "ERROR: ". $sql_query. "<br>" . mysqli_error($link);
-}
+
 ?>
+
+</body>
+
+
+</html>
+
