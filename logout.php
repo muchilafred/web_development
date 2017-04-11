013af1e051ba9dbb7e2558174147d34b06f06ef7
muchilafred <f.m.muchila@rgu.ac.uk>
2017-04-11 17:25:57 +0100

    ytd

---
+++ logout.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 1513133
+ * Date: 11/04/2017
+ * Time: 17:24
+ */
+session_start();
+if (!isset($_SESSION['user'])) {
+    header("location: index.php");
+} else if (isset($_SESSION['user'])!="") {
+    header("location: home.php");
+}
+
+if (isset($_GET['logout'])) {
+    unset($_SESSION['user']);
+    session_unset();
+    session_destroy();
+    header("location: index.php");
+    exit;
+}
