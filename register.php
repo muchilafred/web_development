cfbadb233fee8f1b78c39897078ea78729215c54
muchilafred <f.m.muchila@rgu.ac.uk>
2017-04-11 17:46:23 +0100

    snksn

--- register.php
+++ register.php
@@ -89,7 +89,7 @@ if ( isset($_POST['btn-signup']) ) {
 <html>
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-    <title>Exculsive Fashion</title>
+    <title>Student Management System</title>
     <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" type="text/css"  />
     <link rel="stylesheet" href="style.css" type="text/css" />
 </head>
