ebf82471b7aae8f6289402130b54171da2ec1cca
muchilafred <f.m.muchila@rgu.ac.uk>
2017-04-18 15:36:01 +0100

    tuesday

--- session.php
+++ session.php
@@ -6,37 +6,79 @@
  * Time: 17:23
  */
 
+ob_start();
 session_start();
-include 'dbconnect.php';
-
+require_once 'dbconnect.php';
 if (!isset($_SESSION['user']) ) {
     header("location: index.php");
     exit;
-}
-?>
+}?>
+//User logged in details
+
 
 
 <!DOCTYPE html>
 <html>
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-    <title>Home page</title>
-    <link href="stylesheet1" rel="stylesheet.css" type="text/css" />
+    <title>Welcome - <?php echo $userRow['userEmail']; ?></title>
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootsrap.min.css" type="text/css"  />
+    <link rel="stylesheet" href="style.css" type="text/css" />
 </head>
 <body>
-        <div>
-            <ul>
-                <li><a href="index.php">class</a></li>
-		         <li><a href="session.php">About Us</a></li>
-		         <li><a href="session.php">About Us</a></li>
-		         <li><a href="session.php">About Us</a></li>
-                    <li><a href="session.php">Suggestion</a></li>
-                    <li><a href="session.php">Contact Us</a></li>
-                    <li><a href="logout.php">log out</a></li>
-              
-            </ul>
+
+<nav class="navbar navbar-default navbar-fixed-top">
+    <div class="container">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                <span class ="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="http://www.codingcage.com">Coding cage</a>
         </div>
+        <div id="navbar" class="navbar-collapse collapse">
+            <ul class="nav navbar-nav">
+                <li class="active"><a href="http://www.codingcage.com/2015/01/user-registration-and-login-script-using-php-mysql.html">Back to Article</a></li>
+                <li><a href="http://www.codingcage.com/search/label/jQuery">jQuery</a></li>
+                <li><a href="http://www.codingcage.com/search/label/PHP">PHP</a></li>
+            </ul>
+            <ul class="nav navbar-nav navbar-right">
+
+                <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="fasle">
+                        <span class="glyphicon glyphicon-user"></span>&nbsp;Hi' <?php echo $userRow['userEmail']; ?>&nbsp;<span class ="caret"></span></a>
+                    <ul class="dropdown-menu">
+                        <li><a href="logout.php?logout"><span class="glychicon glyphicon-log-out"></span>&nbsp;Sign Out</a></li>
+                    </ul>
+                </li>
+            </ul>
+        </div><!--/.nav-collapse-->
+    </div>
+</nav>
+
+<div id="wrapper">
+
+    <div class="container">
+
+        <div class="page-header">
+            <h3>Coding Cage - Programming Blog</h3>
+        </div>
+
+        <div class="row">
+            <div class="col-lg-12">
+                <h1>Focuses on PHP, MySQL, Ajax, jQuery, Web Design and more...</h1>
+            </div>
+        </div>
+
+    </div>
+
+</div>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
 
 </body>
 </html>
-
+<?php ob_end_flush(); ?>
