55cf008dbb235b5e578783486156dee1dcf13962
muchilafred <f.m.muchila@rgu.ac.uk>
2017-04-19 18:37:17 +0100

    wednesday

--- admin.login.php
+++ admin.login.php
@@ -4,6 +4,43 @@
 
 
 <head>
+    <style>
+        div.container {
+            width: 100%;
+            border: 1px solid gray;
+        }
+
+        header, footer {
+            padding: 1em;
+            color: white;
+            background-color: mediumslateblue;
+            clear: left;
+            text-align: center;
+        }
+
+        nav {
+            float: left;
+            max-width: 160px;
+            margin: 0;
+            padding: 1em;
+        }
+
+        nav ul {
+            list-style-type: none;
+            padding: 0;
+        }
+
+        nav ul a {
+            text-decoration: none;
+        }
+
+        article {
+            margin-left: 170px;
+            border-left: 1px solid gray;
+            padding: 1em;
+            overflow: hidden;
+        }
+    </style>
 
     <title>Admin login page</title>
 
