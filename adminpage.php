1c02fddc9b9fc6adf2c312d94db89d252abac622
muchilafred <f.m.muchila@rgu.ac.uk>
2017-04-19 15:46:33 +0100

    wednesday

--- adminpage.php
+++ adminpage.php
@@ -17,12 +17,34 @@
 <body>
 <section>
     <h2>List of Students</h2>
-    <?
-    include 'dbConnect.php';
+
+    <table>
+
+        <tr>
+            <th>User-ID</th>
+            <th>User-Name</th>
+            <th>User-email</th>
+            <th>password</th>
+        </tr>
+        </table>
+
+    <?include 'dbConnect.php';
+
     $sql_query = "SELECT userId,userName,userEmail,userPass FROM users; ";
+
     $result = $link->query($sql_query);
+
     while($row = $result->fetch_array()){
+
         // print out fields from row of data
+        echo '<tr>
+                    <td>'.$row['userId'].'</td>
+                    <td>'.$row['userName'].'</td>
+                    <td>'.$row ['userEmail']. '</td>
+                    <td>'.$row ['userPass']. '</td>
+               
+                </tr>';
+
         echo "<p>".$row ['userId']. "  -  ". $row ['userName']."  -  ".$row ['userEmail']."  -  ".$row ['userPass'].$db."</p>";
 
         echo "<p>".$row ['userId']. "  -  ". $row ['userName']."  -  ".$row ['userEmail']."  -  ".$row ['userPass'].$db."</p>";
