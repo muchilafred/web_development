37130e27a730f8d4363c5d059c8017c6a6bf06f5
muchilafred <f.m.muchila@rgu.ac.uk>
2017-04-19 18:46:29 +0100

    wednesday

--- admin.login.php
+++ admin.login.php
@@ -1,60 +1,23 @@
 
-
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 1513133
+ * Date: 18/04/2017
+ * Time: 14:33
+ */
+?>
 <html>
 <head>
-    <style>
-        div.container {
-            width: 100%;
-            border: 1px solid gray;
-        }
-
-        header, footer {
-            padding: 1em;
-            color: white;
-            background-color: mediumslateblue;
-            clear: left;
-            text-align: center;
-        }
-
-        nav {
-            float: left;
-            max-width: 160px;
-            margin: 0;
-            padding: 1em;
-        }
-
-        nav ul {
-            list-style-type: none;
-            padding: 0;
-        }
-
-        nav ul a {
-            text-decoration: none;
-        }
-
-        article {
-            margin-left: 170px;
-            border-left: 1px solid gray;
-            padding: 1em;
-            overflow: hidden;
-        }
-    </style>
 
     <title>Admin login page</title>
 
 </head>
 <body>
-<nav>
-    <ul>
-        <li><a href="#">London</a></li>
-        <li><a href="#">Paris</a></li>
-        <li><a href="#">Tokyo</a></li>
-    </ul>
-</nav>
 
-<h2>Please Login in</h2> <div class="loginBox">
 
-    <h3>Login Form</h3>
+<h2> Login in</h2> <div class="loginBox">
+
     <br ><br >
 
     <form method="post" action="check.php">
