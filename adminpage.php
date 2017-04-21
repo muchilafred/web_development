f6800a36d7fe35dfd07963b36272b01e1791efc5
muchilafred <f.m.muchila@rgu.ac.uk>
2017-04-21 15:04:23 +0100

    thursday

--- adminpage.php
+++ adminpage.php
@@ -16,41 +16,10 @@
 
 <body bgcolor="#1e90ff">
 <section>
-    <h2>List of Students</h2>
+    <h2>Admin view</h2>
 
-    <table>
-        <thead>
-        <tr>
-            <th>User-ID</th>
-            <th>User-Name</th>
-            <th>User-email</th>
-            <th>password</th>
-        </tr>
-        </thead>
-    <tbody>
-    <?include 'dbConnect.php';
-
-    $sql_query = "SELECT userId,userName,userEmail,userPass FROM users; ";
-
-    $result = $link->query($sql_query);
-
-    while($row = $result->fetch_array()){
-
-        // print out fields from row of data
-        echo '<tr><td>'.$row['userId'].'</td>
-                    <td>'.$row['userName'].'</td>
-                   <td>'.$row ['userEmail']. '</td>
-                    <td>'.$row ['userPass']. '</td>
-               
-                </tr>';
-        
-    }
-    $result->close();
-    $link->close();
-    ?>
-    </tbody>
-    </table>
     <ul>
+        <li><a href="student.view.php">Student List</a> </li>
         <li><a href="group1.php">Network Security</a> </li>
         <li><a href="group2.php">Database Administration</li>
         <li><a href="group3.php">Data Computing</li>
